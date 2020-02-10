2b590b891cec9a7b1742ac54529be284604006c4
Lucas <lucaslopezgulare@outlook.com>
2020-02-09 21:59:30 -0300

    First commit.

---
+++ Migrations/20200210005535_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Popularity = table.Column<string>(nullable: true),
+                    AuthorName_First = table.Column<string>(nullable: true),
+                    AuthorName_Last = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Blogs",
+                columns: new[] { "BlogId", "Url" },
+                values: new object[] { 1, "http://model-seeding.com" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "BlogId", "Content", "Popularity", "Title", "AuthorName_First", "AuthorName_Last" },
+                values: new object[] { 1, 1, "First post test", null, "First post", "Carlos", "Carlin" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "BlogId", "Content", "Popularity", "Title", "AuthorName_First", "AuthorName_Last" },
+                values: new object[] { 2, 1, "Second post content", null, "Second post", "Funny", "Furniture" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            #region Custom seeding
+            using (var context = new BlogginContext())
+            {
+                context.Database.Migrate();
+                var blogUrl = "http://custom-seeding.com";
+                var blog = context.Blogs.FirstOrDefault(b => b.Url == blogUrl);                
+                if (blog == null) context.Blogs.Add(new Blog { Url = blogUrl });
+                context.SaveChanges();
+            }
+            #endregion
+
+            #region CRUD operations
+            using (var db = new BlogginContext())
+            {
+                Create(db);
+                var blog = Read(db);
+                Update(db, blog);
+                Delete(db, blog);
+                
+                List(db);
+            }
+            #endregion
+        }
+
+        static void Create(BlogginContext dbContext) 
+        {
+            Console.WriteLine("Inserting new blog");
+
+            var url = "http://blogs.msdn.com/adonet";
+
+            if (dbContext.Blogs.FirstOrDefault(b => b.Url == url) == null)
+                dbContext.Blogs.Add(new Blog
+                {
+                    BlogId = dbContext.Blogs.Last().BlogId + 1,
+                    Url = url
+                });
+
+            dbContext.SaveChanges();
+        }
+
+        static Blog Read(BlogginContext dbContext)
+        {
+            Console.WriteLine("Querying for a blog");
+            var blog = dbContext.Blogs
+                .OrderBy(b => b.BlogId)
+                .Last();
+
+            return blog;
+        }
+
+        static void Update(BlogginContext dbContext, Blog blog)
+        {
+            Console.WriteLine("Updating the blog and adding a post.");
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(
+                new Post 
+                {
+                    Title = "Hello world!",
+                    Content = "I wrote an app using EF Core",
+                    AuthorName = new Name  
+                    {
+                        First = "Lucky",
+                        Last = "Luck"
+                    }
+                });
+            dbContext.SaveChanges();
+        }
+
+        static void Delete(DbContext dbContext, Blog blog)
+        {
+            Console.WriteLine("Delete the blog");
+            dbContext.Remove(blog);
+            dbContext.SaveChanges();
+        }
+
+        static void List(BlogginContext dbContext)
+        {
+            Console.WriteLine();
+            
+            foreach (var blog in dbContext.Blogs.Include(blog => blog.Posts))
+            {
+                Console.WriteLine($"Blog {blog.Url}");
+
+                foreach (var post in blog.Posts)
+                    Console.WriteLine($"\t{post.Title}: {post.Content} by {post.AuthorName.First} {post.AuthorName.Last}");
+            }            
+        }
+    }
+}
